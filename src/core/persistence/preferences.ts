a225ae77ef1ea1c45d0d076aaf54b4318ac16495
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-07-20 16:53:11 -0300

    naming changes

--- src/core/persistence/preferences.ts
+++ src/core/persistence/preferences.ts
@@ -1,10 +1,10 @@
 export class Preferences {
-    public autoSave: boolean;
+    public autoSaveEnabled: boolean;
     public autoSaveInterval: number;
 
     public constructor(data?: any) {
-        this.autoSave = data.autoSaveEnabled ?? true;
-        this.autoSaveInterval = data.autoSaveIntervalInSeconds ?? 60;
+        this.autoSaveEnabled = data.autoSaveEnabled ?? true;
+        this.autoSaveInterval = data.autoSaveInterval ?? 60;
     }
 
     public static fromJson(data: any): Preferences {
@@ -13,7 +13,7 @@ export class Preferences {
 
     public toJson(): any {
         return {
-            autoSave : this.autoSave,
+            autoSaveEnabled : this.autoSaveEnabled,
             autoSaveInterval : this.autoSaveInterval,
         }
     }
