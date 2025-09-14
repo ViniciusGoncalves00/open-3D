3a97eb5db7c254ef6cdeb0c6f66716e6b6cf4dba
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-09-14 17:29:28 -0300

    removed unnused

--- src/core/api/prefab-mesh.ts
+++ src/core/api/prefab-mesh.ts
@@ -1,33 +1,5 @@
 import { Accessor, BufferView, Buffer, Mesh, Primitive } from "../../assets/components/mesh";
 
-export class MeshBufferDescriptor {
-	public readonly typeBytes;
-	public readonly elementsByVertex;
-	public readonly colorOffset;
-	public readonly uvOffset;
-	public readonly vertexCount;
-	public readonly positionOffset;
-	public readonly vertices;
-
-	public readonly vertexByteSize;
-    public readonly colorBytesOffset;
-    public readonly UVBytesOffset;
-
-	public constructor(typeBytes: number, elementsByVertex: number, colorOffset: number, uvOffset: number, vertexCount: number, positionOffset: number, vertices: Float32Array) {
-		this.typeBytes = typeBytes;
-		this.elementsByVertex = elementsByVertex;
-		this.colorOffset = colorOffset;
-		this.uvOffset = uvOffset;
-		this.vertexCount = vertexCount;
-		this.positionOffset = positionOffset;
-		this.vertices = vertices;
-
-		this.vertexByteSize = typeBytes * elementsByVertex;
-    	this.colorBytesOffset = typeBytes * colorOffset;
-    	this.UVBytesOffset = typeBytes * uvOffset;
-	}
-}
-
 export class PrefabMesh {
     public static quad(size: number = 1, color?: [number, number, number]): Mesh {
     	const half = size / 2;
@@ -99,9 +71,9 @@ export class PrefabMesh {
     	const vertexBufferView = new BufferView(vertexBuffer, 0, positions.byteLength);
     	const indexBufferView = new BufferView(indexBuffer, 0, indices.byteLength);
 		
-    	const positionAccessor = new Accessor(vertexBufferView, 5126, positions.length / 3, "VEC3", 0, 24); // FLOAT32
+    	const positionAccessor = new Accessor(vertexBufferView, 5126, positions.length / 3, "VEC3", 0, 24);
 		const colorAccessor = new Accessor(vertexBufferView, 5126, 4, "VEC3", 12, 24);
-    	const indexAccessor = new Accessor(indexBufferView, 5125, indices.length, "SCALAR");        // UINT32
+    	const indexAccessor = new Accessor(indexBufferView, 5125, indices.length, "SCALAR");
 
     	const primitive = new Primitive({
     	    POSITION: positionAccessor,
